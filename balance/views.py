7a0fc10ca058b6b2e30559dea836cdd33a02f8e4
irena <irena.vent@gmail.com>
2021-10-03 17:53:33 +0200

    UPDATE movement done

--- balance/views.py
+++ balance/views.py
@@ -83,4 +83,41 @@ def borrar(id):
             return redirect(url_for("inicio"))
         return redirect(url_for("inicio"))
 
+@app.route("/modificar/<int:id>", methods=['GET', 'POST'])
+def modificar(id):
+    formulario = MovimientoForm()
 
+    if request.method == "GET":
+
+        consulta = """
+        SELECT id, fecha, concepto, ingreso_gasto, cantidad 
+          FROM movimientos
+        WHERE id = ?;
+    """
+        try:
+            movimientos = dbManager.consultaSQL(consulta,[id])
+        except Exception as e:
+            print ("Se ha producido un error de acceso a base de datos", e)
+            flash("Se ha producido un error en la base de datos")
+            return render_template("nuevo_movimiento.html", form=formulario)
+        
+        if len(movimientos) == 0:
+            flash(f"Movimiento {id} no encontrado")
+            return redirect(url_for("inicio"))
+
+        el_movimiento = movimientos[0]
+        el_movimiento["fecha"] = date.fromisoformat(el_movimiento["fecha"])
+        formulario = MovimientoForm(data = el_movimiento)
+
+        return render_template("modificar_movimiento.html", form=formulario, id=el_movimiento["id"])
+    else: 
+        consulta = """
+        UPDATE movimientos SET fecha = :fecha, concepto = :concepto, ingreso_gasto = :ingreso_gasto WHERE id = :id;
+    """
+        try:
+            movimiento = dbManager.modificaSQL(consulta, formulario.data)
+        except Exception as e:
+            print ("Se ha producido un error de acceso a base de datos", e)
+            flash("Se ha producido un error en la base de datos")        
+            return redirect(url_for("inicio"))
+        return redirect(url_for("inicio"))
